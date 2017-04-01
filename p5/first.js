d8c58b856bcc248caeff4c54d9bc11d7a531a71f
standfordshruti <shrutirungta04@gmail.com>
2017-04-01 14:18:01 +0530

    Update first.js

--- p5/first.js
+++ p5/first.js
@@ -1,9 +1,27 @@
 function setup() { 
-  createCanvas(400, 400);
-}
+  createCanvas(700, 700);
+	x=100;
+	y=100;
+} 
 
+var y;
+var x;
 function draw() { 
-   background(220);
-   ellipse(10, 10, 10);
+	background(220);
+	arc(x, y, 80, 80, 0.4, 5.4, PIE);
+	if (x<200) {
+		ellipse(200,100,20,20)
+	}
+	if (x<300) {
+		ellipse(300,100,20,20)
+	}
+	if (x<400) {
+		ellipse(400,100,20,20)
+	}
+	if (x<500) {
+		ellipse(500,100,20,20)
+	}
+	
+	x+=2
 }
 
